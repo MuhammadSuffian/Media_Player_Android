187b5b0d4d9c6f88d70e032c1a7626cd28b9dc75
Muhammad Suffian Tafoor <112380672+MuhammadSuffian@users.noreply.github.com>
2025-01-07 09:43:10 +0500

    v1

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.mediaplayer;
 
+import static android.media.AudioManager.STREAM_MUSIC;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +17,8 @@ import com.example.mediaplayer.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+//    AudioManager audio;
+    MediaPlayer mediaplayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,17 +27,32 @@ public class MainActivity extends AppCompatActivity {
         void initlization(){
             binding=ActivityMainBinding.inflate(getLayoutInflater());
             setContentView(binding.getRoot());
+            mediaplayer=new MediaPlayer();
+            mediaplayer.setAudioStreamType(STREAM_MUSIC);
+            initMusic();
             buttonsLogic();
         }
         void buttonsLogic(){
         binding.btnPlay.setOnClickListener(v->{
-
+            mediaplayer.start();
         });
-        binding.btnPlay.setOnClickListener(v->{
-
+        binding.btnPause.setOnClickListener(v->{
+            mediaplayer.pause();
             });
-        binding.btnPlay.setOnClickListener(v->{
-
+        binding.btnStop.setOnClickListener(v->{
+            mediaplayer.stop();
+            mediaplayer.reset();
+            initMusic();
             });
+        }
+        void initMusic(){
+            String offlinemusic="android.resourse://"+getPackageName()+"/"+R.raw.baby_cry;
+            Uri uri=Uri.parse(offlinemusic);
+            try{
+                mediaplayer.setDataSource(this,uri);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
         }
 }
