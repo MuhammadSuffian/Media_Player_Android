ebedd3e65d720bee5a588d0c8b978f2314a03218
Muhammad Suffian Tafoor <112380672+MuhammadSuffian@users.noreply.github.com>
2025-01-07 09:14:36 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mediaplayer;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mediaplayer.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initlization();
+    }
+        void initlization(){
+            binding=ActivityMainBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
+        }
+}
